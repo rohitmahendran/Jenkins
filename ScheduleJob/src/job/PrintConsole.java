c03e85897e5b00b97ae49ea814d0ec271de5821c
rohitmahendran <34469717+rohitmahendran@users.noreply.github.com>
2020-10-18 11:16:42 +0530

    Update PrintConsole.java

--- ScheduleJob/src/job/PrintConsole.java
+++ ScheduleJob/src/job/PrintConsole.java
@@ -5,6 +5,7 @@ public class PrintConsole {
 	public static void main(String[] args) {
 		
 		System.out.println("Hurray, the second Jenkins Job");
+		System.out.println("Hurray Rohit, the third Jenkins Job");
 
 	}
 
